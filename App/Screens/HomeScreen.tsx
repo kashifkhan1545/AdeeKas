5f76a2c635b39536d4405de0c8bbd348494b0f9a
Kashif Khan <kashifkhan26262@gmail.com>
2025-02-28 15:14:07 +0500

    startin own bussiness

---
+++ App/Screens/HomeScreen.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Welcome to AdeeKas
+            (The Shirts Expert)</Text>
+      
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    width:'100%',
+    textAlign:'center'
+  },
+  
+});
+
+export default HomeScreen;
